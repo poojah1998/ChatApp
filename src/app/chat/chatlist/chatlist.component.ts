680670a911d8b0f8029fadf57ffa6f18fef96e60
poojah1998 <poojashree@apie.tech>
2022-11-04 09:33:41 +0530

    .

--- src/app/chat/chatlist/chatlist.component.ts
+++ src/app/chat/chatlist/chatlist.component.ts
@@ -222,7 +222,7 @@ export class ChatlistComponent implements OnInit, AfterViewChecked {
     this.sidenav.openLeftNav();
   }
 
-
+                                                                                                                                                           
 
 
 }
