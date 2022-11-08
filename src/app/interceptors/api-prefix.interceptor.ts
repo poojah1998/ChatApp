7446859ddf4310859f033d8c77df434b47e33efb
bkumar <bkumar@optimuminfo.com>
2022-11-08 10:16:45 +0530

    .

--- src/app/interceptors/api-prefix.interceptor.ts
+++ src/app/interceptors/api-prefix.interceptor.ts
@@ -16,7 +16,7 @@ export class ApiPrefixInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 const token="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNzZkZGY5MzA1MzQzMjE0YzZmOTBiYyIsImJyYW5jaGlkIjoiNjA3NmRkZjkzMDUzNDMyMTRjNmY5MGJjIiwibmFtZSI6IkRocnV2IENoaGFicmEiLCJlbWFpbCI6ImRocnV2LmNoaGFicmFhQGdtYWlsLmNvbSIsIm1vYmlsZSI6Ijk5NTM1OTQzNDgiLCJSb2xlIjoiU3VwZXIiLCJpYXQiOjE2NjU0ODU3NTJ9.Lm0I0WKtXz6lHbTMF4fLfyKpRdcSrdT92wOwDXK-HJA"
     if (token) {
-      console.log(token,"hgggggggggggggggggggggggg");
+      // console.log(token,"hgggggggggggggggggggggggg");
       request = request.clone({
         setHeaders: {
           authorization: `${token}`
